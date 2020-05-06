a4681f39c043dc21163feea29718710c2701564d
Ivan Georgiev <ivan.georgiev@upnetix.com>
2020-05-06 20:30:44 +0300

    first commit

---
+++ Test/Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Ivan Georgiev on 6.05.20.
+//  Copyright © 2020 Upnetix AD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
